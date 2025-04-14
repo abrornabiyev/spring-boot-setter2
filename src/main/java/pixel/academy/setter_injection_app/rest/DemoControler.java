725c7caaed6da06003ff508c90dbbd9ae16a8414
Пользователь <abror.nabiyev321@gmail.com>
2025-04-14 21:52:46 +0300

    commit

--- src/main/java/pixel/academy/setter_injection_app/rest/DemoControler.java
+++ src/main/java/pixel/academy/setter_injection_app/rest/DemoControler.java
@@ -1,6 +1,7 @@
 package pixel.academy.setter_injection_app.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pixel.academy.setter_injection_app.common.Chef;
@@ -11,7 +12,7 @@ public class DemoControler {
     private Chef myChef;
 
     @Autowired
-    public DemoControler(Chef theChef) {
+    public DemoControler(@Qualifier("italianChef") Chef theChef) {
         myChef = theChef;
     }
 
